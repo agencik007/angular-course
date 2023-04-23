0e9e9471eec9248755f66bedbfc924ef4877c257
Karol Jasnoch <agencik007@gmail.com>
2023-04-23 09:21:54 +0200

    updated project

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Ingredients } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredients[] = [
+    {
+      name: 'Apples',
+      amount: 5,
+    },
+    {
+      name: 'Tomatoes',
+      amount: 10,
+    },
+  ];
 }
